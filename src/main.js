938adbbc0f0ba0d7f757311aeab4dffb0f6eebb3
Afanasjew Vladislav <afanasjew.vladick@yandex.ru>
2021-09-05 19:08:53 +0300

    #fix: failed to compile

--- src/main.js
+++ src/main.js
@@ -3,11 +3,6 @@ import App from './App.vue'
 import store from './store/index'
 import router from "./router"
 
-import Dashboard from './components/dashboard.vue'
-
-
-
-
 new Vue({
   el: '#app',
   router,
